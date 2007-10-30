62922ad9ae4b44171e691c17d4c668d3ea6643dd
ktchu <none@none>
2007-10-30 18:55:23 +0000

    Added support for second-order discretization of Eikonal equation solver.
    --HG--
    extra : convert_revision : svn%3A8ecc5fa7-3a31-0410-acfe-e558a581ced7/trunk%4032

--- src/serial/lsm_FMM_eikonal2d.c
+++ src/serial/lsm_FMM_eikonal2d.c
@@ -60,9 +60,27 @@ void FMM_initializeFront_Eikonal2d(
 /* 
  * FMM_updateGridPoint_Eikonal2d_Order1() computes and returns the 
  * updated phi value of the specified grid point using values of 
- * neighbors that have status "KNOWN". 
+ * neighbors that have status "KNOWN" and a first-order accurate 
+ * discretization of the gradient operator.
  */
 double FMM_updateGridPoint_Eikonal2d_Order1(
+  FMM_CoreData *fmm_core_data,
+  FMM_FieldData *fmm_field_data,
+  int *grid_idx,
+  int num_dims,
+  int *grid_dims,
+  double *dx);
+
+/* 
+ * FMM_updateGridPoint_Eikonal2d_Order2() computes and returns the 
+ * updated phi value of the specified grid point using values of 
+ * neighbors that have status "KNOWN" and a second-order accurate 
+ * discretization of the gradient operator when a sufficient number 
+ * of "KNOWN" neighboring grid points are available.  When there are
+ * an insufficient number of "KNOWN" neighbors, the discretization
+ * of the gradient drops to first-order accuracy.
+ */
+double FMM_updateGridPoint_Eikonal2d_Order2(
   FMM_CoreData *fmm_core_data,
   FMM_FieldData *fmm_field_data,
   int *grid_idx,
@@ -104,12 +122,7 @@ int solveEikonalEquation2d(
   if (spatial_derivative_order == 1) {
     updateGridPoint = &FMM_updateGridPoint_Eikonal2d_Order1;
   } else if (spatial_derivative_order == 2) {
-/*  KTC - ADD LATER
     updateGridPoint = &FMM_updateGridPoint_Eikonal2d_Order2;
-*/
-    fprintf(stderr,
-           "ERROR: second-order spatial derivatives currently unsupported\n");
-    return LSM_FMM_2D_ERR_INVALID_SPATIAL_DERIVATIVE_ORDER;
   } else {
     fprintf(stderr,
            "ERROR: Invalid spatial derivative order.  Only first-\n");
@@ -255,8 +268,8 @@ double FMM_updateGridPoint_Eikonal2d_Order1(
   double *speed = fmm_field_data->speed;
 
   /* variables used in phi update */
-  PointStatus  neighbor_status;
-  double phi_upwind[LSM_FMM_2D_NDIM];
+  PointStatus neighbor_status;
+  double phi_upwind;
   double phi_plus;
   double inv_dx_sq; 
   int offset[LSM_FMM_2D_NDIM]; 
@@ -289,7 +302,7 @@ double FMM_updateGridPoint_Eikonal2d_Order1(
     }
 
     /* find "upwind" direction and phi value */
-    phi_upwind[dir] = DBL_MAX;
+    phi_upwind = DBL_MAX;
 
     /* check minus direction */
     offset[dir] = -1;
@@ -299,7 +312,7 @@ double FMM_updateGridPoint_Eikonal2d_Order1(
       idx_neighbor = LSM_FMM_2D_IDX(neighbor[0],neighbor[1]);
       neighbor_status = (PointStatus) gridpoint_status[idx_neighbor];
       if (KNOWN == neighbor_status) {
-        phi_upwind[dir] = phi[idx_neighbor];
+        phi_upwind = phi[idx_neighbor];
       }
     }
 
@@ -320,21 +333,193 @@ double FMM_updateGridPoint_Eikonal2d_Order1(
          * NOTE: to avoid having to use the C math library,
          *       we define our own ABS macro
          */
-        if (LSM_FMM_2D_ABS(phi_plus) < LSM_FMM_2D_ABS(phi_upwind[dir])) {
-          phi_upwind[dir] = phi_plus;
+        if (LSM_FMM_2D_ABS(phi_plus) < LSM_FMM_2D_ABS(phi_upwind)) {
+          phi_upwind = phi_plus;
         }
       }
     }
 
     /*
-     * accumulate coefficients for phi if either of the neighbors are "known"
+     * accumulate coefficients for phi if either of the neighbors are "KNOWN"
      */
-    if (phi_upwind[dir] < DBL_MAX) {
+    if (phi_upwind < DBL_MAX) {
       /* accumulate coefs for phi */ 
       inv_dx_sq = 1/dx[dir]/dx[dir];
       phi_A += inv_dx_sq;
-      phi_B += phi_upwind[dir]*inv_dx_sq;
-      phi_C += phi_upwind[dir]*phi_upwind[dir]*inv_dx_sq;
+      phi_B += inv_dx_sq*phi_upwind;
+      phi_C += inv_dx_sq*phi_upwind*phi_upwind;
+    }
+
+  } /* loop over coordinate directions */
+
+  /* complete computation of phi_B and phi_C */
+  phi_B *= -2.0;
+  phi_C -= 1/speed[idx_cur_gridpoint]/speed[idx_cur_gridpoint];
+
+  /* compute phi by solving quadratic equation */
+  discriminant = phi_B*phi_B - 4*phi_A*phi_C;
+  phi_updated = DBL_MAX;
+  if (discriminant >= 0) {
+    phi_updated = (-phi_B + sqrt(discriminant))/2/phi_A;
+  } else {
+    fprintf(stderr,"ERROR: phi update - discriminant negative!!!\n");
+  }
+
+  /* set phi at current grid point */
+  phi[idx_cur_gridpoint] = phi_updated;
+
+  return phi_updated;
+}
+
+
+double FMM_updateGridPoint_Eikonal2d_Order2(
+  FMM_CoreData *fmm_core_data,
+  FMM_FieldData *fmm_field_data,
+  int *grid_idx,
+  int num_dims,
+  int *grid_dims,
+  double *dx)
+{
+  int *gridpoint_status = FMM_Core_getGridPointStatusDataArray(fmm_core_data);
+
+  /* FMM Field Data variables */
+  double *phi   = fmm_field_data->phi; 
+  double *speed = fmm_field_data->speed;
+
+  /* variables used in phi update */
+  PointStatus neighbor_status;
+  double phi_upwind1, phi_upwind2;
+  double phi_plus;
+  int second_order_switch;
+  double inv_dx_sq; 
+  int offset[LSM_FMM_2D_NDIM]; 
+  int neighbor1[LSM_FMM_2D_NDIM];
+  int neighbor2[LSM_FMM_2D_NDIM];
+
+  /* coefficients of quadratic equation for phi */
+  double phi_A = 0;
+  double phi_B = 0;
+  double phi_C = 0;
+  double discriminant;
+  double phi_updated;
+
+  /* auxilliary variables */
+  int dir;  /* loop variable for spatial directions */
+  int l;    /* extra loop variable */ 
+  int idx_cur_gridpoint, idx_neighbor1, idx_neighbor2;
+
+  /* unused function parameters */
+  (void) num_dims;
+
+  /* compute index for current grid point */
+  idx_cur_gridpoint = LSM_FMM_2D_IDX(grid_idx[0],grid_idx[1]);
+
+  /* calculate update to phi */
+  for (dir = 0; dir < LSM_FMM_2D_NDIM; dir++) { /* loop over coord directions */
+
+    /* reset offset */
+    for (l = 0; l < LSM_FMM_2D_NDIM; l++) { 
+      offset[l] = 0; 
+    }
+
+    /* reset phi_upwind1 and phi_upwind2 to DBL_MAX */
+    phi_upwind1 = DBL_MAX;
+    phi_upwind2 = DBL_MAX;
+
+    /* reset second_order_switch to 0 (i.e. assume there are not enough */
+    /* KNOWN neighbors for second-order discretization.                 */
+    second_order_switch = 0;
+
+    /* check minus direction */
+    offset[dir] = -1;
+    neighbor1[0] = grid_idx[0] + offset[0];
+    neighbor1[1] = grid_idx[1] + offset[1];
+    neighbor2[0] = grid_idx[0] + 2*offset[0];
+    neighbor2[1] = grid_idx[1] + 2*offset[1];
+    if (!LSM_FMM_2D_IDX_OUT_OF_BOUNDS(neighbor1[0],neighbor1[1],grid_dims)) {
+      idx_neighbor1 = LSM_FMM_2D_IDX(neighbor1[0],neighbor1[1]);
+      neighbor_status = (PointStatus) gridpoint_status[idx_neighbor1];
+      if (KNOWN == neighbor_status) {
+        phi_upwind1 = phi[idx_neighbor1];
+
+        /* check for neighbor required for second-order accuracy */
+        if (!LSM_FMM_2D_IDX_OUT_OF_BOUNDS(neighbor2[0],neighbor2[1],
+                                          grid_dims)) {
+          idx_neighbor2 = LSM_FMM_2D_IDX(neighbor2[0],neighbor2[1]);
+          neighbor_status = (PointStatus) gridpoint_status[idx_neighbor2];
+          if ( (KNOWN == neighbor_status) &&
+               (LSM_FMM_2D_ABS(phi_upwind2) < LSM_FMM_2D_ABS(phi_upwind1)) ) {
+            phi_upwind2 = phi[idx_neighbor2];
+            second_order_switch = 1;
+          }
+        } 
+
+      } /* end case: first-order neighbor is KNOWN */
+    } 
+
+    /* check plus direction */
+    offset[dir] = 1;
+    neighbor1[0] = grid_idx[0] + offset[0];
+    neighbor1[1] = grid_idx[1] + offset[1];
+    neighbor2[0] = grid_idx[0] + 2*offset[0];
+    neighbor2[1] = grid_idx[1] + 2*offset[1];
+    if (!LSM_FMM_2D_IDX_OUT_OF_BOUNDS(neighbor1[0],neighbor1[1],grid_dims)) {
+      idx_neighbor1 = LSM_FMM_2D_IDX(neighbor1[0],neighbor1[1]);
+      neighbor_status = (PointStatus) gridpoint_status[idx_neighbor1];
+      if (KNOWN == neighbor_status) {
+        phi_plus = phi[idx_neighbor1];
+
+        /* 
+         * choosing the upwind direction to be the direction
+         * with the smaller abs(phi) value gives a consistent 
+         * solution to the "upwind" Eikonal equation.
+         * NOTE: to avoid having to use the C math library,
+         *       we define our own ABS macro
+         */
+        if (LSM_FMM_2D_ABS(phi_plus) < LSM_FMM_2D_ABS(phi_upwind1)) {
+          phi_upwind1 = phi_plus;
+          phi_upwind2 = DBL_MAX;
+          second_order_switch = 0;
+
+          /* check for neighbor required for second-order accuracy */
+          if (!LSM_FMM_2D_IDX_OUT_OF_BOUNDS(neighbor2[0],neighbor2[1],
+                                            grid_dims)) {
+            idx_neighbor2 = LSM_FMM_2D_IDX(neighbor2[0],neighbor2[1]);
+            neighbor_status = (PointStatus) gridpoint_status[idx_neighbor2];
+            if ( (KNOWN == neighbor_status) &&
+                 (LSM_FMM_2D_ABS(phi_upwind2) < LSM_FMM_2D_ABS(phi_upwind1)) ) {
+              phi_upwind2 = phi[idx_neighbor2];
+              second_order_switch = 1;
+            }
+          } 
+
+        }  /* end if: plus is upwind direction */
+      } /* end case: first-order neighbor is KNOWN */
+    }
+
+    /*
+     * accumulate coefficients for phi if either of the neighbors are "KNOWN"
+     */
+    if (phi_upwind1 < DBL_MAX) {
+      /* temporary variables */
+      double one_plus_switch_over_two = 1.0+second_order_switch/2.0;
+      double phi_upwind_contrib;
+
+      /* set phi_upwind_contrib to be first- or second-order */
+      /* contribution based on value of second_order_switch  */
+      if (second_order_switch == 1) {
+        phi_upwind_contrib = phi_upwind1+second_order_switch*phi_upwind1
+                           - second_order_switch/2.0*phi_upwind2;
+      } else {
+        phi_upwind_contrib = phi_upwind1;
+      }
+
+      /* accumulate coefs for phi */ 
+      inv_dx_sq = 1/dx[dir]/dx[dir];
+      phi_A += inv_dx_sq*one_plus_switch_over_two*one_plus_switch_over_two;
+      phi_B += inv_dx_sq*one_plus_switch_over_two
+                        *phi_upwind_contrib;
+      phi_C += inv_dx_sq*phi_upwind_contrib*phi_upwind_contrib;
     }
 
   } /* loop over coordinate directions */
