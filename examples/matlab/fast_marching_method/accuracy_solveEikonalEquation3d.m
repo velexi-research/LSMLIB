231632a2f4843b492e76a21fa30d12465b3d627e
ktchu <none@none>
2007-11-01 13:10:50 +0000

    ENHANCEMENTS:  Added example programs that demonstrate the order of accuracy of Eikonal equation solvers in 2d and 3d.
    --HG--
    extra : convert_revision : svn%3A8ecc5fa7-3a31-0410-acfe-e558a581ced7/trunk%4040

---
+++ examples/matlab/fast_marching_method/accuracy_solveEikonalEquation3d.m
@@ -0,0 +1,234 @@
+%
+% File:        accuracy_solveEikonalEquation3d.m
+% Copyright:   (c) 2005-2008 Kevin T. Chu
+% Revision:    $Revision: 1.4 $
+% Modified:    $Date: 2006/08/13 15:45:27 $
+% Description: MATLAB test code for solveEikonalEquation3d MEX file
+%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% This script checks the order of accuracy for the 
+% solveEikonalEquation3d MATLAB MEX-function.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% setup environment
+clear
+format long
+
+% set zero tolerance
+zero_tol = 1000*eps;
+
+% grid sizes
+grid_sizes = [25, 50, 100, 200];
+
+% allocate space for errors
+errs1_first_order = zeros(size(grid_sizes));
+errs1_second_order = zeros(size(grid_sizes));
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Test Problem 1
+% --------------
+%  * boundary: phi = 0 on circle centered at (0,0,0) with radius 0.2
+%  * speed:    1
+%  * mask:     interior of circle
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% problem parameters
+center = [0.0,0.0,0.0]; 
+radius = 0.2;
+
+% loop over grid sizes
+for i = 1:length(grid_sizes)
+
+  % construct grid 
+  N = grid_sizes(i);
+  x_lo = -1; x_hi = 1;
+  y_lo = -1; y_hi = 1;
+  z_lo = -1; z_hi = 1;
+  dx = (x_hi-x_lo)/N;
+  dy = (y_hi-y_lo)/N;
+  dz = (z_hi-z_lo)/N;
+  x = (x_lo:dx:x_hi)';
+  y = (y_lo:dy:y_hi)';
+  z = (z_lo:dz:z_hi)';
+  dX = [dx dy dz];
+  [X,Y,Z] = meshgrid(x,y,z);  
+
+  % display some information for user ...
+  disp_str = sprintf('   solving Test Problem 1 with N = %d', N);
+  disp(disp_str);
+
+  % compute exact solution
+  phi_exact = sqrt(X.^2 + Y.^2 + Z.^2) - radius;
+  idx_interior = find( ...
+      sqrt((X-center(1)).^2+(Y-center(2)).^2+(Z-center(3)).^2) - radius ...
+    < zero_tol);
+  phi_exact(idx_interior) = 0;
+
+  boundary_data = -1*ones(size(X));
+  idx_bdry = find( ...
+      abs(sqrt((X-center(1)).^2+(Y-center(2)).^2+(Z-center(3)).^2) - radius) ...
+    < (1+zero_tol)*max(dX)); 
+  boundary_data(idx_bdry) = sqrt( (X(idx_bdry)-center(1)).^2 ...
+                                + (Y(idx_bdry)-center(2)).^2 ...
+                                + (Z(idx_bdry)-center(3)).^2 ) - radius;
+  speed = ones(size(boundary_data));
+  mask = ones(size(boundary_data));
+  mask(idx_interior) = -1;
+ 
+  % solve for phi using first-order discretization
+  spatial_discretization_order = 1;
+  phi = solveEikonalEquation3d(boundary_data, speed, dX, mask, ...
+                               spatial_discretization_order);
+  phi(idx_interior) = 0;
+
+  errs1_first_order(i) = max(max(max(abs(phi-phi_exact))));
+
+  % solve for phi using second-order discretization
+  spatial_discretization_order = 2;
+  phi = solveEikonalEquation3d(boundary_data, speed, dX, mask, ...
+                               spatial_discretization_order);
+  phi(idx_interior) = 0;
+
+  errs1_second_order(i) = max(max(max(abs(phi-phi_exact))));
+
+end
+
+% plot results
+figure(1); clf;
+P_first_order = polyfit(log(grid_sizes),log(errs1_first_order),1);
+order_first_order = -P_first_order(1);
+N_plot = [10, 1000];
+loglog(N_plot,exp(log(N_plot)*P_first_order(1)+P_first_order(2)),'k');
+hold on;
+plot(grid_sizes,errs1_first_order,'bo','MarkerSize',14,'MarkerFaceColor','b');
+title('Test Problem 1: First-Order Scheme');
+order_str = sprintf('Order = %1.1f', order_first_order);
+text(100,0.05,order_str);
+xlabel('N');
+ylabel('L_\infty Error');
+
+% plot results
+figure(2); clf;
+P_second_order = polyfit(log(grid_sizes),log(errs1_second_order),1);
+order_second_order = -P_second_order(1);
+N_plot = [10, 1000];
+loglog(N_plot,exp(log(N_plot)*P_second_order(1)+P_second_order(2)),'k');
+hold on;
+plot(grid_sizes,errs1_second_order,'bo','MarkerSize',14,'MarkerFaceColor','b');
+title('Test Problem 1: Second-Order Scheme');
+order_str = sprintf('Order = %1.1f', order_second_order);
+text(100,1e-2,order_str);
+xlabel('N');
+ylabel('L_\infty Error');
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Test Problem 2
+% --------------
+%  * boundary: phi = 1 on circle centered at (0,0,0) with radius 0.7
+%  * speed:    1
+%  * mask:     exterior of circle centered at (0,0,0) with radius 0.7
+%          and interior of circle centered at (0,0,0) with radius 0.25
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% problem parameters
+center = [0.0,0.0,0.0]; 
+inner_radius = 0.25;
+outer_radius = 0.7;
+
+% loop over grid sizes
+for i = 1:length(grid_sizes)
+
+  % construct grid 
+  N = grid_sizes(i);
+  x_lo = -1; x_hi = 1;
+  y_lo = -1; y_hi = 1;
+  z_lo = -1; z_hi = 1;
+  dx = (x_hi-x_lo)/N;
+  dy = (y_hi-y_lo)/N;
+  dz = (z_hi-z_lo)/N;
+  x = (x_lo:dx:x_hi)';
+  y = (y_lo:dy:y_hi)';
+  z = (z_lo:dz:z_hi)';
+  dX = [dx dy dz];
+  [X,Y,Z] = meshgrid(x,y,z);  
+
+  % display some information for user ...
+  disp_str = sprintf('   solving Test Problem 2 with N = %d', N);
+  disp(disp_str);
+
+  % compute exact solution
+  phi_exact = 1 - (sqrt(X.^2 + Y.^2 + Z.^2) - outer_radius);
+  idx_exterior = find( ...
+      ( sqrt((X-center(1)).^2+(Y-center(2)).^2+(Z-center(3)).^2) ...
+      - outer_radius > -zero_tol )...
+   | ( sqrt((X-center(1)).^2+(Y-center(2)).^2+(Z-center(3)).^2) ...
+      - inner_radius < zero_tol ) );
+  phi_exact(idx_exterior) = 0;
+
+  boundary_data = -1*ones(size(X));
+  idx_bdry = find( ...
+      abs( sqrt((X-center(1)).^2+(Y-center(2)).^2+(Z-center(3)).^2) ...
+         - outer_radius ) ...
+    < (1+zero_tol)*max(dX)); 
+  boundary_data(idx_bdry) = 1 - ( sqrt( (X(idx_bdry)-center(1)).^2 ...
+                                      + (Y(idx_bdry)-center(2)).^2 ...
+                                      + (Z(idx_bdry)-center(3)).^2 ) ...
+                                - outer_radius);
+  speed = ones(size(boundary_data));
+  mask = ones(size(boundary_data));
+  mask(idx_exterior) = -1;
+ 
+  % solve for phi using first-order discretization
+  spatial_discretization_order = 1;
+  phi = solveEikonalEquation3d(boundary_data, speed, dX, mask, ...
+                               spatial_discretization_order);
+  phi(idx_exterior) = 0;
+
+  errs2_first_order(i) = max(max(max(abs(phi-phi_exact))));
+
+  % solve for phi using second-order discretization
+  spatial_discretization_order = 2;
+  phi = solveEikonalEquation3d(boundary_data, speed, dX, mask, ...
+                               spatial_discretization_order);
+  phi(idx_exterior) = 0;
+
+  errs2_second_order(i) = max(max(max(abs(phi-phi_exact))));
+
+end
+
+% plot results
+figure(3); clf;
+P_first_order = polyfit(log(grid_sizes),log(errs2_first_order),1);
+order_first_order = -P_first_order(1);
+N_plot = [10, 1000];
+loglog(N_plot,exp(log(N_plot)*P_first_order(1)+P_first_order(2)),'k');
+hold on;
+plot(grid_sizes,errs2_first_order,'bo','MarkerSize',14,'MarkerFaceColor','b');
+title('Test Problem 2: First-Order Scheme');
+order_str = sprintf('Order = %1.1f', order_first_order);
+text(100,0.05,order_str);
+xlabel('N');
+ylabel('L_\infty Error');
+
+% plot results
+figure(4); clf;
+P_second_order = polyfit(log(grid_sizes),log(errs2_second_order),1);
+order_second_order = -P_second_order(1);
+N_plot = [10, 1000];
+loglog(N_plot,exp(log(N_plot)*P_second_order(1)+P_second_order(2)),'k');
+hold on;
+plot(grid_sizes,errs2_second_order,'bo','MarkerSize',14,'MarkerFaceColor','b');
+title('Test Problem 2: Second-Order Scheme');
+order_str = sprintf('Order = %1.1f', order_second_order);
+text(100,1e-2,order_str);
+xlabel('N');
+ylabel('L_\infty Error');
