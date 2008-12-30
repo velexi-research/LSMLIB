2956187d142ef7f6602efbb680e3e6d9315e58c0
masha <none@none>
2008-12-30 00:05:00 +0000

    new LSM3D_MAX_NORM_DIFF_CONTROL_VOLUME_LOCAL        LSM3D_COMPUTE_STABLE_NORMAL_VEL_DT_LOCAL        LSM3D_COMPUTE_STABLE_CONST_NORMAL_VEL_DT_LOCAL
    --HG--
    extra : convert_revision : svn%3A8ecc5fa7-3a31-0410-acfe-e558a581ced7/trunk%40128

--- src/toolbox/utilities/lsm_utilities3d_local.h
+++ src/toolbox/utilities/lsm_utilities3d_local.h
@@ -31,14 +31,35 @@ extern "C" {
  *      C/C++ code                     Fortran code
  *      ----------                     ------------
  */
-#define LSM3D_MAX_NORM_DIFF_LOCAL            lsm3dmaxnormdifflocal_
-#define LSM3D_COMPUTE_STABLE_ADVECTION_DT_LOCAL                                   \
+#define LSM3D_MAX_NORM_DIFF_LOCAL      lsm3dmaxnormdifflocal_
+
+#define LSM3D_COMPUTE_STABLE_ADVECTION_DT_LOCAL                           \
                                        lsm3dcomputestableadvectiondtlocal_
 #define LSM3D_COMPUTE_STABLE_NORMAL_VEL_DT_LOCAL                                  \
                                        lsm3dcomputestablenormalveldtlocal_
-#define LSM3D_COMPUTE_STABLE_CONST_NORMAL_VEL_DT_LOCAL                      \
-                                        lsm3dcomputestableconstnormalveldtlocal_
+#define LSM3D_COMPUTE_STABLE_NORMAL_VEL_DT_CONTROL_VOLUME_LOCAL            \
+                          lsm3dcomputestablenormalveldtcontrolvolumelocal_
+#define LSM3D_COMPUTE_STABLE_CONST_NORMAL_VEL_DT_LOCAL                     \
+                                  lsm3dcomputestableconstnormalveldtlocal_
 
+/*!
+*
+*  LSM3D_MAX_NORM_DIFF_LOCAL() computes the max norm of the difference 
+*  between the two specified scalar fields.
+*  The routine loops only over local (narrow band) points. 
+*
+*  Arguments:
+*    max_norm_diff (out):  max norm of the difference between the fields
+*    field1 (in):          scalar field 1
+*    field2 (in):          scalar field 2
+*    index_*](in):         coordinates of local (narrow band) points
+*    n*_index(in):         index range of points in index_*
+*    *_gb (in):            index range for ghostbox
+*    narrow_band(in):      array that marks voxels outside desired fillbox
+*    mark_fb(in):          upper limit narrow band value for voxels in 
+*                          fillbox
+*
+*/
 void LSM3D_MAX_NORM_DIFF_LOCAL(
   LSMLIB_REAL *max_norm_diff,
   const LSMLIB_REAL *field1,
@@ -69,6 +90,23 @@ void LSM3D_MAX_NORM_DIFF_LOCAL(
   const int *khi_nb_gb,
   const unsigned char *mark_fb);
 
+/*!
+*
+*  LSM3D_COMPUTE_STABLE_ADVECTION_DT_LOCAL() computes the stable time step size 
+*  for an advection term based on a CFL criterion.
+*  
+*  Arguments:
+*    dt (out):           step size
+*    vel_* (in):         components of velocity at t = t_cur
+*    *_gb (in):          index range for ghostbox
+*    dx, dy (in):        grid spacing
+*    index_*(in):        coordinates of local (narrow band) points
+*    n*_index(in):       index range of points in index_*
+*    narrow_band(in):    array that marks voxels outside desired fillbox
+*    mark_fb(in):        upper limit narrow band value for voxels in 
+*                        fillbox
+*
+*/
 void LSM3D_COMPUTE_STABLE_ADVECTION_DT_LOCAL(
   LSMLIB_REAL *dt,
   const LSMLIB_REAL *vel_x,
@@ -97,7 +135,35 @@ void LSM3D_COMPUTE_STABLE_ADVECTION_DT_LOCAL(
   const int *klo_nb_gb,
   const int *khi_nb_gb,
   const unsigned char *mark_fb);
-
+  
+  
+/*!
+*
+*  LSM3D_COMPUTE_STABLE_NORMAL_VEL_DT_LOCAL() computes the stable time step 
+*  size for a normal velocity term based on a CFL criterion.
+*  
+*  Arguments:
+*    dt (out):             step size
+*    vel_n (in):           normal velocity at t = t_cur
+*    phi_*_plus (in):      components of forward approx to grad(phi) at 
+*                          t = t_cur
+*    phi_*_minus (in):     components of backward approx to grad(phi) at
+*                          t = t_cur
+*    *_gb (in):            index range for ghostbox
+*    d*(in):               grid spacing
+*    index_*(in):          coordinates of local (narrow band) points
+*    n*_index(in):         index range of points in index_*
+*    narrow_band(in):      array that marks voxels outside desired fillbox
+*    mark_fb(in):          upper limit narrow band value for voxels in 
+*                          fillbox
+*
+*  NOTES:
+*   - max(phi_*_plus , phi_*_minus) is the value of phi_* that is 
+*     used in the time step size calculation.  This may be more 
+*     conservative than necessary for Godunov's method, but it is 
+*     cheaper to compute.
+*
+*/
 void LSM3D_COMPUTE_STABLE_NORMAL_VEL_DT_LOCAL(
   LSMLIB_REAL *dt,
   const LSMLIB_REAL *vel_n,
@@ -142,7 +208,115 @@ void LSM3D_COMPUTE_STABLE_NORMAL_VEL_DT_LOCAL(
   const int *klo_nb_gb,
   const int *khi_nb_gb,
   const unsigned char *mark_fb);
-    
+
+/*!
+*
+*  LSM3D_COMPUTE_STABLE_NORMAL_VEL_DT_CONTROL_VOLUME_LOCAL() computes the stable time step 
+*  size for a normal velocity term based on a CFL criterion.
+*  
+*  Arguments:
+*    dt (out):             step size
+*    vel_n (in):           normal velocity at t = t_cur
+*    phi_*_plus (in):      components of forward approx to grad(phi) at 
+*                          t = t_cur
+*    phi_*_minus (in):     components of backward approx to grad(phi) at
+*                          t = t_cur
+*    *_gb (in):            index range for ghostbox
+*    d*(in):               grid spacing
+*    index_*(in):          coordinates of local (narrow band) points
+*    n*_index(in):         index range of points in index_*
+*    narrow_band(in):      array that marks voxels outside desired fillbox
+*    mark_fb(in):          upper limit narrow band value for voxels in 
+*                          fillbox
+*
+*  NOTES:
+*   - max(phi_*_plus , phi_*_minus) is the value of phi_* that is 
+*     used in the time step size calculation.  This may be more 
+*     conservative than necessary for Godunov's method, but it is 
+*     cheaper to compute.
+*
+*/
+void LSM3D_COMPUTE_STABLE_NORMAL_VEL_DT_CONTROL_VOLUME_LOCAL(
+  LSMLIB_REAL *dt,
+  const LSMLIB_REAL *vel_n,
+  const int *ilo_vel_gb, 
+  const int *ihi_vel_gb,
+  const int *jlo_vel_gb, 
+  const int *jhi_vel_gb,
+  const int *klo_vel_gb, 
+  const int *khi_vel_gb,
+  const LSMLIB_REAL *phi_x_plus,
+  const LSMLIB_REAL *phi_y_plus,
+  const LSMLIB_REAL *phi_z_plus,
+  const int *ilo_grad_phi_plus_gb, 
+  const int *ihi_grad_phi_plus_gb,
+  const int *jlo_grad_phi_plus_gb, 
+  const int *jhi_grad_phi_plus_gb,
+  const int *klo_grad_phi_plus_gb, 
+  const int *khi_grad_phi_plus_gb,
+  const LSMLIB_REAL *phi_x_minus,
+  const LSMLIB_REAL *phi_y_minus,
+  const LSMLIB_REAL *phi_z_minus,
+  const int *ilo_grad_phi_minus_gb, 
+  const int *ihi_grad_phi_minus_gb,
+  const int *jlo_grad_phi_minus_gb, 
+  const int *jhi_grad_phi_minus_gb,
+  const int *klo_grad_phi_minus_gb, 
+  const int *khi_grad_phi_minus_gb,
+  const LSMLIB_REAL *control_vol,
+  const int *ilo_control_vol_gb, 
+  const int *ihi_control_vol_gb,
+  const int *jlo_control_vol_gb, 
+  const int *jhi_control_vol_gb,
+  const int *klo_control_vol_gb, 
+  const int *khi_control_vol_gb,
+  const int *control_vol_sgn,
+  const LSMLIB_REAL *dx,
+  const LSMLIB_REAL *dy,
+  const LSMLIB_REAL *dz,
+  const LSMLIB_REAL *cfl_number,
+  const int *index_x,
+  const int *index_y, 
+  const int *index_z,
+  const int *nlo_index,
+  const int *nhi_index,
+  const unsigned char *narrow_band,
+  const int *ilo_nb_gb,
+  const int *ihi_nb_gb,
+  const int *jlo_nb_gb,
+  const int *jhi_nb_gb,
+  const int *klo_nb_gb,
+  const int *khi_nb_gb,
+  const unsigned char *mark_fb);
+  
+  
+/*!
+*
+* LSM3D_COMPUTE_STABLE_CONST_NORMAL_VEL_DT_LOCAL() computes the stable time 
+* step size for a normal velocity term based on a CFL criterion.
+*  
+*  Arguments:
+*    dt (out):          step size
+*    vel_n (in):        normal velocity at t = t_cur, constant for all pts
+*    phi_*_plus (in):   components of forward approx to grad(phi) at 
+*                       t = t_cur
+*    phi_*_minus (in):  components of backward approx to grad(phi) at
+*                       t = t_cur
+*    index_*(in):       coordinates of local (narrow band) points
+*    n*_index(in):      index range of points in index_*
+*    narrow_band(in):   array that marks voxels outside desired fillbox
+*    mark_fb(in):       upper limit narrow band value for voxels in 
+*                       fillbox
+*    *_gb (in):         index range for ghostbox
+*    d* (in):           grid spacing
+*
+*  NOTES:
+*   - max(phi_*_plus , phi_*_minus) is the value of phi_* that is 
+*     used in the time step size calculation.  This may be more 
+*     conservative than necessary for Godunov's method, but it is 
+*     cheaper to compute.
+*
+*/     
 void LSM3D_COMPUTE_STABLE_CONST_NORMAL_VEL_DT_LOCAL(
   LSMLIB_REAL *dt,
   const LSMLIB_REAL *vel_n,
