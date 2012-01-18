7d14c6cc2596c7717412e78f44f2406b03a0097b
Daniel Wheeler <daniel.wheeler2@gmail.com>
2012-01-18 16:37:42 +0000

    Calculation of the distance function with a fast marching method now works.
    git-svn-id: svn+ssh://code.matforge.org/wd15/trunk/lsmlib@1441 01ab6b12-1d45-0410-8926-c5867bd4e26f

--- pylsmlib/pylsmlib.pyx
+++ pylsmlib/pylsmlib.pyx
@@ -3,15 +3,15 @@ import numpy as np
 from numpy.core import intc	
 cimport numpy as np
 from numpy.compat import asbytes
-from numpy import int, double
+from numpy import int, double, int32
 from pylsmlib cimport computeDistanceFunction2d
 
 def computeDistanceFunction2d_(phi, nx=1, ny=1, dx=1., dy=1.):
     cdef np.ndarray[double, ndim=1] _phi = phi.copy()
     cdef np.ndarray[double, ndim=1] distance_function = np.zeros((nx * ny,))
-    cdef np.ndarray[double, ndim=0] mask = np.array(0.0)
+    cdef np.ndarray[double, ndim=1] mask = np.zeros((nx * ny,))
     cdef int spatial_derivative_order = 2							
-    cdef np.ndarray[long int, ndim=1] grid_dims = np.array((nx, ny), dtype=int)
+    cdef np.ndarray[int, ndim=1] grid_dims = np.array((nx, ny), dtype=int32)
     cdef np.ndarray[double, ndim=1] dX = np.array((dx, dy))
     
     error = computeDistanceFunction2d(
--- pylsmlib/test.py
+++ pylsmlib/test.py
@@ -9,8 +9,6 @@ pyximport.install(setup_args = {'options' :
 
 
 from pylsmlib import computeDistanceFunction2d_
-                     
-
 
 N = 100
 L = 1.
@@ -20,9 +18,7 @@ phi = CellVariable(mesh=m)
 phi[:] = (m.x - L / 2)**2 + (m.y - L / 2)**2 - 0.25**2
 vi = Viewer(phi)
 vi.plot()
-
 phi[:] = computeDistanceFunction2d_(phi.value, nx=m.nx, ny=m.ny, dx=m.dx, dy=m.dy)
-
 vi.plot()
-raw_input()
+raw_input('stopped')
 
