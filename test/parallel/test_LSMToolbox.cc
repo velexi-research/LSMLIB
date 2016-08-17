249a8c416a8ea656f7dd23774b78531f3c671dea
Kevin Chu <ktchu@serendipityresearch.org>
2016-08-16 20:25:51 -0700

    Add skeleton test framework.
    Former-commit-id: 82185157674acfa246de302c60bfba59b0e831d9

---
+++ test/parallel/test_LSMToolbox.cc
@@ -0,0 +1,20 @@
+// --- Includes
+
+// Google Test
+#include "gtest/gtest.h"
+
+// LSMLIB
+// TODO
+//
+// Test case: TODO
+TEST(UpdateGTest, BasicUsage)
+{
+    // --- Preparations
+
+    // --- Exercise functionality
+
+    // --- Check results
+
+    // Status code
+    ASSERT_TRUE(true);
+}
