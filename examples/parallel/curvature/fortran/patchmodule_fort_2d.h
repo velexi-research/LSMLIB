90be359fcbc943ae8fd817099c19e7c3fb56bec2
Kevin Chu <kevin@velexi.com>
2017-08-29 12:02:34 -0700

    Add star shaped initial interface for parallel 2D curvature example.
    Former-commit-id: 1b73fa32be04d0866749250053ed72cd05f9998b

--- examples/parallel/curvature/fortran/patchmodule_fort_2d.h
+++ examples/parallel/curvature/fortran/patchmodule_fort_2d.h
@@ -1,6 +1,6 @@
 /*
  * File:        patchmodule_fort.h
- * Description: Header file for patch module routines for 2d LSM example 
+ * Description: Header file for patch module routines for 2d LSM example
  *              problem
  */
 
@@ -16,9 +16,25 @@
  *      ----------            ------------
  */
 #define INIT_CIRCLE           initcircle_
+#define INIT_STAR             initstar_
 
 void INIT_CIRCLE(
-  const LSMLIB_REAL* level_set,
+  const LSMLIB_REAL* phi,
+  const int* ilo_gb,
+  const int* ihi_gb,
+  const int* jlo_gb,
+  const int* jhi_gb,
+  const int* ilo_fb,
+  const int* ihi_fb,
+  const int* jlo_fb,
+  const int* jhi_fb,
+  const LSMLIB_REAL* x_lower,
+  const LSMLIB_REAL* dx,
+  const LSMLIB_REAL* center,
+  const LSMLIB_REAL* radius);
+
+void INIT_STAR(
+  const LSMLIB_REAL* phi,
   const int* ilo_gb,
   const int* ihi_gb,
   const int* jlo_gb,
