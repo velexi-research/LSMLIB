560f0ef640077d3c7415d6be94196596cdb9e90f
Kevin Chu <kevin@velexi.com>
2021-09-09 22:38:28 -0700

    Add unit tests for calculus toolbox.

--- tests/test_calculus_toolbox.cc
+++ tests/test_calculus_toolbox.cc
@@ -1,50 +1,108 @@
 /*
- * File:        test_calculus_toolbox.c
- * Copyrights:  (c) 2005 The Trustees of Princeton University and Board of
- *                  Regents of the University of Texas.  All rights reserved.
- *              (c) 2009 Kevin T. Chu.  All rights reserved.
- * Revision:    $Revision$
- * Modified:    $Date$
- * Description: Demo code for delta and heaviside functions
+ * test_calculus_toolbox.cc
+ *
+ * Test program for delta and heaviside functions
+ *
+ * ---------------------------------------------------------------------
+ * COPYRIGHT/LICENSE. This file is part of the LSMLIB package. It is
+ * subject to the license terms in the LICENSE file found in the
+ * top-level directory of this distribution. No part of the LSMLIB
+ * package, including this file, may be copied, modified, propagated,
+ * or distributed except according to the terms contained in the
+ * LICENSE file.
+ * ---------------------------------------------------------------------
  */
 
 #include <stdio.h>
-#include "lsmlib_config.h"
-#include "lsm_calculus_toolbox.h"
 
-int main(void)
-{
+#include <gtest/gtest.h>
 
-  LSMLIB_REAL h[20], d[20];
-  LSMLIB_REAL x[20];
-  LSMLIB_REAL d_phi;
-  LSMLIB_REAL eps;
-  int i;
+#include "lsmlib/lsmlib_config.h"
+#include "lsmlib/lsm_calculus_toolbox.h"
 
-  /* initialize d_phi and eps (smoothing width) */
-  d_phi = 0.1;
-  eps = 3.0*d_phi;
+/*
+ * Test fixtures
+ */
+class LSMCalculusToolboxTest : public ::testing::Test {
+    protected:
+        // Data members
+        LSMLIB_REAL x[20];
+        LSMLIB_REAL d_phi;
+        LSMLIB_REAL eps;
 
-  /* initialize x */
-  for (i = 0; i < 20; i++) x[i] = -1.0 + i*d_phi;
 
-  /* compute smoothed heaviside and delta functions */
-  for (i = 0; i < 20; i++) {
-    h[i] = LSM_HEAVISIDE(x[i],eps);
-    d[i] = LSM_DELTA_FUNCTION(x[i],eps);
-  }
+        // Constructor
+        LSMCalculusToolboxTest(){
+            // Set d_phi and eps (smoothing width)
+            d_phi = 0.1;
+            eps = 3.0 * d_phi;
 
-  /* output smoothed heaviside function */
-  printf("Smoothed Heaviside Function:\n");
-  for (i = 0; i < 20; i++) 
-    printf("%f, ",h[i]);
-  printf("\n");
+            // Set grid values
+            for (int i = 0; i < 20; i++) {
+                x[i] = -1.0 + i * d_phi;
+            }
+    }
+};
 
-  /* output smoothed delta-function */
-  printf("Smoothed Delta-Function:\n");
-  for (i = 0; i < 20; i++) 
-    printf("%f, ",d[i]);
-  printf("\n");
+/*
+ * Tests
+ */
+TEST_F(LSMCalculusToolboxTest, Heaviside) {
+    LSMLIB_REAL h[20];
+    LSMLIB_REAL h_expected[20] = {
+        0.000000000000000,
+        0.000000000000000,
+        0.000000000000000,
+        0.000000000000000,
+        0.000000000000000,
+        0.000000000000000,
+        0.000000000000000,
+        0.000000000000000,
+        0.028834442811219,
+        0.195501109477885,
+        0.500000000000000,
+        0.804498890522115,
+        0.971165557188782,
+        1.000000000000000,
+        1.000000000000000,
+        1.000000000000000,
+        1.000000000000000,
+        1.000000000000000,
+        1.000000000000000,
+        1.000000000000000};
 
-  return 0;  
+    // Check values of smoothed Heaviside function
+    for (int i = 0; i < 20; i++) {
+        EXPECT_NEAR(LSM_HEAVISIDE(x[i], eps), h_expected[i], 1e-6);
+    }
+}
+
+TEST_F(LSMCalculusToolboxTest, DeltaFunction) {
+    LSMLIB_REAL d[20];
+    LSMLIB_REAL d_expected[20] = {
+        0.000000000000000,
+        0.000000000000000,
+        0.000000000000000,
+        0.000000000000000,
+        0.000000000000000,
+        0.000000000000000,
+        0.000000000000000,
+        0.000000000000000,
+        0.833333333333334,
+        2.500000000000000,
+        3.333333333333333,
+        2.499999999999999,
+        0.833333333333331,
+        0.000000000000000,
+        0.000000000000000,
+        0.000000000000000,
+        0.000000000000000,
+        0.000000000000000,
+        0.000000000000000,
+        0.000000000000000};
+
+    // Check values of smoothed delta function
+    for (int i = 0; i < 20; i++) {
+        EXPECT_NEAR(LSM_DELTA_FUNCTION(x[i], eps), d_expected[i], 1e-6);
+    }
 }
