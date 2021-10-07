d371b94bafbb9ed4186b98eca3acfdbc9d2aa699
Kevin Chu <kevin@velexi.com>
2021-10-06 18:46:19 -0700

    Update unit tests for 2D Neumann boundary conditions.

--- tests/boundary_conditions/test_neumann_bc_3d.cc
+++ tests/boundary_conditions/test_neumann_bc_3d.cc
@@ -359,3 +359,245 @@ TEST_F(LSMBoundaryConditions3DTest, ENO1) {
         }
     }
 }
+
+TEST_F(LSMBoundaryConditions3DTest, ENO2) {
+    // variable declarations
+    int i, j, k;
+    int bdry_location_idx;
+
+    // allocate memory for divided differences
+    D1 = new LSMLIB_REAL[ghostbox_dims[0] * ghostbox_dims[1] *
+                         ghostbox_dims[2]];
+    D2 = new LSMLIB_REAL[ghostbox_dims[0] * ghostbox_dims[1] *
+                         ghostbox_dims[2]];
+
+    // impose homogeneous Neumann boundary conditions
+    bdry_location_idx = 0;
+    LSM3D_HOMOGENEOUS_NEUMANN_ENO2(
+        phi,
+        &ghostbox_lower[0],
+        &ghostbox_upper[0],
+        &ghostbox_lower[1],
+        &ghostbox_upper[1],
+        &ghostbox_lower[2],
+        &ghostbox_upper[2],
+        &box_lower[0],
+        &box_upper[0],
+        &box_lower[1],
+        &box_upper[1],
+        &box_lower[2],
+        &box_upper[2],
+        &bdry_location_idx);
+
+    bdry_location_idx = 1;
+    LSM3D_HOMOGENEOUS_NEUMANN_ENO2(
+        phi,
+        &ghostbox_lower[0],
+        &ghostbox_upper[0],
+        &ghostbox_lower[1],
+        &ghostbox_upper[1],
+        &ghostbox_lower[2],
+        &ghostbox_upper[2],
+        &box_lower[0],
+        &box_upper[0],
+        &box_lower[1],
+        &box_upper[1],
+        &box_lower[2],
+        &box_upper[2],
+        &bdry_location_idx);
+
+    bdry_location_idx = 2;
+    LSM3D_HOMOGENEOUS_NEUMANN_ENO2(
+        phi,
+        &ghostbox_lower[0],
+        &ghostbox_upper[0],
+        &ghostbox_lower[1],
+        &ghostbox_upper[1],
+        &ghostbox_lower[2],
+        &ghostbox_upper[2],
+        &box_lower[0],
+        &box_upper[0],
+        &box_lower[1],
+        &box_upper[1],
+        &box_lower[2],
+        &box_upper[2],
+        &bdry_location_idx);
+
+    bdry_location_idx = 3;
+    LSM3D_HOMOGENEOUS_NEUMANN_ENO2(
+        phi,
+        &ghostbox_lower[0],
+        &ghostbox_upper[0],
+        &ghostbox_lower[1],
+        &ghostbox_upper[1],
+        &ghostbox_lower[2],
+        &ghostbox_upper[2],
+        &box_lower[0],
+        &box_upper[0],
+        &box_lower[1],
+        &box_upper[1],
+        &box_lower[2],
+        &box_upper[2],
+        &bdry_location_idx);
+
+    bdry_location_idx = 4;
+    LSM3D_HOMOGENEOUS_NEUMANN_ENO2(
+        phi,
+        &ghostbox_lower[0],
+        &ghostbox_upper[0],
+        &ghostbox_lower[1],
+        &ghostbox_upper[1],
+        &ghostbox_lower[2],
+        &ghostbox_upper[2],
+        &box_lower[0],
+        &box_upper[0],
+        &box_lower[1],
+        &box_upper[1],
+        &box_lower[2],
+        &box_upper[2],
+        &bdry_location_idx);
+
+    bdry_location_idx = 5;
+    LSM3D_HOMOGENEOUS_NEUMANN_ENO2(
+        phi,
+        &ghostbox_lower[0],
+        &ghostbox_upper[0],
+        &ghostbox_lower[1],
+        &ghostbox_upper[1],
+        &ghostbox_lower[2],
+        &ghostbox_upper[2],
+        &box_lower[0],
+        &box_upper[0],
+        &box_lower[1],
+        &box_upper[1],
+        &box_lower[2],
+        &box_upper[2],
+        &bdry_location_idx);
+
+    // compute spatial derivatives using ENO2
+    LSM3D_HJ_ENO2(
+        phi_x_plus, phi_y_plus, phi_z_plus,
+        &ghostbox_lower[0],
+        &ghostbox_upper[0],
+        &ghostbox_lower[1],
+        &ghostbox_upper[1],
+        &ghostbox_lower[2],
+        &ghostbox_upper[2],
+        phi_x_minus, phi_y_minus, phi_z_minus,
+        &ghostbox_lower[0],
+        &ghostbox_upper[0],
+        &ghostbox_lower[1],
+        &ghostbox_upper[1],
+        &ghostbox_lower[2],
+        &ghostbox_upper[2],
+        phi,
+        &ghostbox_lower[0],
+        &ghostbox_upper[0],
+        &ghostbox_lower[1],
+        &ghostbox_upper[1],
+        &ghostbox_lower[2],
+        &ghostbox_upper[2],
+        D1,
+        &ghostbox_lower[0],
+        &ghostbox_upper[0],
+        &ghostbox_lower[1],
+        &ghostbox_upper[1],
+        &ghostbox_lower[2],
+        &ghostbox_upper[2],
+        D2,
+        &ghostbox_lower[0],
+        &ghostbox_upper[0],
+        &ghostbox_lower[1],
+        &ghostbox_upper[1],
+        &ghostbox_lower[2],
+        &ghostbox_upper[2],
+        &box_lower[0],
+        &box_upper[0],
+        &box_lower[1],
+        &box_upper[1],
+        &box_lower[2],
+        &box_upper[2],
+        &dx, &dy, &dz);
+
+    /*
+     * check that derivatives normal to faces of computational domain are 0.
+     */
+
+    // x-lower
+    i = box_lower[0];
+    for (k = 0; k < box_dims[2]; k++) {
+        for (j = 0; j < box_dims[1]; j++) {
+            int idx = (i+ghostcell_width)
+                    + (j+ghostcell_width)*ghostbox_dims[0]
+                    + (k+ghostcell_width)*ghostbox_dims[0]*ghostbox_dims[1];
+
+            LSMLIB_REAL err_x_lower = fabs(phi_x_minus[idx]);
+            EXPECT_NEAR(err_x_lower, 0, 1e-6);
+       }
+   }
+
+    // x-upper
+    i = box_upper[0];
+    for (k = 0; k < box_dims[2]; k++) {
+        for (j = 0; j < box_dims[1]; j++) {
+            int idx = (i+ghostcell_width)
+                    + (j+ghostcell_width)*ghostbox_dims[0]
+                    + (k+ghostcell_width)*ghostbox_dims[0]*ghostbox_dims[1];
+
+            LSMLIB_REAL err_x_upper = fabs(phi_x_plus[idx]);
+            EXPECT_NEAR(err_x_upper, 0, 1e-6);
+        }
+    }
+
+    // y-lower
+    j = box_lower[1];
+    for (k = 0; k < box_dims[2]; k++) {
+        for (i = 0; i < box_dims[0]; i++) {
+            int idx = (i+ghostcell_width)
+                    + (j+ghostcell_width)*ghostbox_dims[0]
+                    + (k+ghostcell_width)*ghostbox_dims[0]*ghostbox_dims[1];
+
+            LSMLIB_REAL err_y_lower = fabs(phi_y_minus[idx]);
+            EXPECT_NEAR(err_y_lower, 0, 1e-6);
+        }
+    }
+
+    // y-upper
+    j = box_upper[1];
+    for (k = 0; k < box_dims[2]; k++) {
+        for (i = 0; i < box_dims[0]; i++) {
+            int idx = (i+ghostcell_width)
+                    + (j+ghostcell_width)*ghostbox_dims[0]
+                    + (k+ghostcell_width)*ghostbox_dims[0]*ghostbox_dims[1];
+
+            LSMLIB_REAL err_y_upper = fabs(phi_y_plus[idx]);
+            EXPECT_NEAR(err_y_upper, 0, 1e-6);
+        }
+    }
+
+    // z-lower
+    k = box_lower[2];
+    for (j = 0; j < box_dims[1]; j++) {
+        for (i = 0; i < box_dims[0]; i++) {
+            int idx = (i+ghostcell_width)
+                    + (j+ghostcell_width)*ghostbox_dims[0]
+                    + (k+ghostcell_width)*ghostbox_dims[0]*ghostbox_dims[1];
+
+            LSMLIB_REAL err_z_upper = fabs(phi_z_minus[idx]);
+            EXPECT_NEAR(err_z_upper, 0, 1e-6);
+       }
+   }
+
+    // z-upper
+    k = box_upper[2];
+    for (j = 0; j < box_dims[1]; j++) {
+        for (i = 0; i < box_dims[0]; i++) {
+            int idx = (i+ghostcell_width)
+                    + (j+ghostcell_width)*ghostbox_dims[0]
+                    + (k+ghostcell_width)*ghostbox_dims[0]*ghostbox_dims[1];
+
+            LSMLIB_REAL err_z_upper = fabs(phi_z_plus[idx]);
+            EXPECT_NEAR(err_z_upper, 0, 1e-6);
+        }
+    }
+}
